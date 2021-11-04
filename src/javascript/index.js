a832af9775377ac9e8c160057c2abb50268e9e74
Anton-Sorkin <anton.sorkin98@outlook.com>
2021-11-04 14:00:10 +0100

    fixed so on sumbit, input becomes a todo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,26 @@
-window.onload = () => ({
-	//Selections
-	//EventListener
-});
-//Functions
+window.onload = () => {
+	let submitForm = document.querySelector(".submitForm");
+	submitForm.addEventListener("click", addTodo);
+};
+
+function addTodo(e) {
+	e.preventDefault();
+
+	let listOfTodos = document.querySelector(".listOfTodos");
+	let userInput = document.querySelector(".userInput");
+
+	let todoContainer = document.createElement("div");
+	todoContainer.classList.add("todo-containter");
+
+	let createTodoLi = document.createElement("li");
+	createTodoLi.innerText = userInput.value;
+	createTodoLi.classList.add("todo-li");
+
+	let removeButton = document.createElement("button");
+	removeButton.innerText = "%"; //can create html and display it
+	removeButton.classList.add("remove-btn");
+
+	createTodoLi.appendChild(removeButton);
+	todoContainer.appendChild(createTodoLi);
+	listOfTodos.appendChild(todoContainer);
+}
